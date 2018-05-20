ada019e689c63bd3c589df808cecaa53ed7ee3dd
crossoverJie <crossoverjie@gmail.com>
2018-05-21 00:23:01 +0800

    :tada: Initial commit.

---
+++ netty-action-heartbeat/src/main/java/com/crossoverjie/netty/action/Application.java
@@ -0,0 +1,16 @@
+package com.crossoverjie.netty.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author crossoverJie
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+	}
+
+}
