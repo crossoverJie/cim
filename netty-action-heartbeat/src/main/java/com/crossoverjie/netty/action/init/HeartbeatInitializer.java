4caf7d1a5840bf7696f01533028e9219a34c9a36
crossoverJie <crossoverjie@gmail.com>
2018-05-28 00:47:04 +0800

    :bulb: Documenting source code.

--- netty-action-heartbeat/src/main/java/com/crossoverjie/netty/action/init/HeartbeatInitializer.java
+++ netty-action-heartbeat/src/main/java/com/crossoverjie/netty/action/init/HeartbeatInitializer.java
@@ -17,7 +17,7 @@ public class HeartbeatInitializer extends ChannelInitializer<Channel> {
     @Override
     protected void initChannel(Channel ch) throws Exception {
         ch.pipeline()
-                //五秒没有收到消息
+                //五秒没有收到消息 将IdleStateHandler 添加到 ChannelPipeline 中
                 .addLast(new IdleStateHandler(5, 0, 0))
                 .addLast(new HeartbeatDecoder())
                 .addLast(new HeartBeatSimpleHandle());
