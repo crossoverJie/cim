d577b24646ec441e731c033ea7b4203c2c9643cc
crossoverJie <crossoverjie@gmail.com>
2018-12-24 14:49:15 +0800

    :construction_worker: 添加 CI 构建到系统

--- script/route-startup.sh
+++ script/route-startup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nohup java -jar  /root/work/route/cim-forward-route-1.0.0-SNAPSHOT.jar  > log.file 2>&1 &
+nohup java -jar  /root/work/route/cim-forward-route-1.0.0-SNAPSHOT.jar  > /root/work/route/log.file 2>&1 &
