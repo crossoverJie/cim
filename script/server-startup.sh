88fd4371569491b6ea936dc16a6556aa9067d11f
crossoverJie <crossoverjie@gmail.com>
2018-12-24 16:29:10 +0800

    :construction_worker: 添加 CI 构建到系统

--- script/server-startup.sh
+++ script/server-startup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nohup java -jar  /root/work/server/cim-server-1.0.0-SNAPSHOT.jar  > /root/work/server/log.file 2>&1 &
+nohup java -jar  /root/work/server/cim-server-1.0.0-SNAPSHOT.jar --cim.server.port=9000  > /root/work/server/log.file 2>&1 &
