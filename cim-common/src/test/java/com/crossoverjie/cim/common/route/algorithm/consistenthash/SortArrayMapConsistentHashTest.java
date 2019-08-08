755e2e984b9c753b16de66d72501de513b193bc4
baidawei <baidawei-sc@bestpay.com.cn>
2019-08-08 15:40:30 +0800

    将客户端地址改变

--- cim-common/src/test/java/com/crossoverjie/cim/common/route/algorithm/consistenthash/SortArrayMapConsistentHashTest.java
+++ cim-common/src/test/java/com/crossoverjie/cim/common/route/algorithm/consistenthash/SortArrayMapConsistentHashTest.java
@@ -56,27 +56,27 @@ public class SortArrayMapConsistentHashTest {
         AbstractConsistentHash map = new SortArrayMapConsistentHash() ;
 
         List<String> strings = new ArrayList<String>();
-        strings.add("45.78.28.220:9000:8081") ;
-        strings.add("45.78.28.220:9100:9081") ;
+        strings.add("118.89.23.102:9000:8081") ;
+        strings.add("118.89.23.102:9100:9081") ;
 
         String process = map.process(strings,"1551253899106");
 
         System.out.println(process);
-        Assert.assertEquals("45.78.28.220:9000:8081",process);
+        Assert.assertEquals("118.89.23.102:9000:8081",process);
     }
     @Test
     public void getFirstNodeValue5() {
         AbstractConsistentHash map = new SortArrayMapConsistentHash() ;
 
         List<String> strings = new ArrayList<String>();
-        strings.add("45.78.28.220:9000:8081") ;
-        strings.add("45.78.28.220:9100:9081") ;
-        strings.add("45.78.28.220:9100:10081") ;
+        strings.add("118.89.23.102:9000:8081") ;
+        strings.add("118.89.23.102:9100:9081") ;
+        strings.add("118.89.23.102:9100:10081") ;
 
         String process = map.process(strings,"1551253899106");
 
         System.out.println(process);
-        Assert.assertEquals("45.78.28.220:9000:8081",process);
+        Assert.assertEquals("118.89.23.102:9000:8081",process);
     }
 
     @Test
@@ -84,29 +84,29 @@ public class SortArrayMapConsistentHashTest {
         AbstractConsistentHash map = new SortArrayMapConsistentHash() ;
 
         List<String> strings = new ArrayList<String>();
-        strings.add("45.78.28.220:9000:8081") ;
-        strings.add("45.78.28.220:9100:9081") ;
-        strings.add("45.78.28.220:9100:10081") ;
+        strings.add("118.89.23.102:9000:8081") ;
+        strings.add("118.89.23.102:9100:9081") ;
+        strings.add("118.89.23.102:9100:10081") ;
 
         String process = map.process(strings,"1551253899106");
 
         System.out.println(process);
-        Assert.assertEquals("45.78.28.220:9000:8081",process);
+        Assert.assertEquals("118.89.23.102:9000:8081",process);
     }
     @Test
     public void getFirstNodeValue7() {
         AbstractConsistentHash map = new SortArrayMapConsistentHash() ;
 
         List<String> strings = new ArrayList<String>();
-        strings.add("45.78.28.220:9000:8081") ;
-        strings.add("45.78.28.220:9100:9081") ;
-        strings.add("45.78.28.220:9100:10081") ;
-        strings.add("45.78.28.220:9100:00081") ;
+        strings.add("118.89.23.102:9000:8081") ;
+        strings.add("118.89.23.102:9100:9081") ;
+        strings.add("118.89.23.102:9100:10081") ;
+        strings.add("118.89.23.102:9100:00081") ;
 
         String process = map.process(strings,"1551253899106");
 
         System.out.println(process);
-        Assert.assertEquals("45.78.28.220:9000:8081",process);
+        Assert.assertEquals("118.89.23.102:9000:8081",process);
     }
 
 
