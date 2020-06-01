b89c086641383fc7a584256c2aa29fdd7696653d
crossoverJie <crossoverjie@gmail.com>
2020-06-01 23:32:15 +0800

    script

--- script/server-startup.sh
+++ script/server-startup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nohup java -jar  /data/work/cim/server/cim-server-1.0.0-SNAPSHOT.jar --cim.server.port=9000  > /data/work/cim/server/log.file 2>&1 &
+nohup java -jar  /data/work/cim/server/cim-server-1.0.0-SNAPSHOT.jar --cim.server.port=9000 --app.zk.addr=127.0.0.1:2182  > /data/work/cim/server/log.file 2>&1 &
