e65a745b2a26bf2486b1f4b1941fdda4259900ef
crossoverJie <crossoverjie@gmail.com>
2020-06-02 00:22:09 +0800

    :art: Improving structure / format of the code.

--- cim-common/src/main/java/com/crossoverjie/cim/common/util/StringUtil.java
+++ cim-common/src/main/java/com/crossoverjie/cim/common/util/StringUtil.java
@@ -31,37 +31,23 @@ public class StringUtil {
     }
 
     public static boolean isValidIPAddress(String ip) {
-
-        // Regex for digit from 0 to 255.
         String zeroTo255
                 = "(\\d{1,2}|(0|1)\\"
                 + "d{2}|2[0-4]\\d|25[0-5])";
-
-        // Regex for a digit from 0 to 255 and
-        // followed by a dot, repeat 4 times.
-        // this is the regex to validate an IP address.
         String regex
                 = zeroTo255 + "\\."
                 + zeroTo255 + "\\."
                 + zeroTo255 + "\\."
                 + zeroTo255;
 
-        // Compile the ReGex
         Pattern p = Pattern.compile(regex);
 
-        // If the IP address is empty
-        // return false
         if (ip == null) {
             return false;
         }
 
-        // Pattern class contains matcher() method
-        // to find matching between given IP address
-        // and regular expression.
         Matcher m = p.matcher(ip);
 
-        // Return if the IP address
-        // matched the ReGex
         return m.matches();
     }
 }
