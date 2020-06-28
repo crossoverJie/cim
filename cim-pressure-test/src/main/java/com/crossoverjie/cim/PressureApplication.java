4a178367847c58ea2b9e78e66cb6fad5f8a81cb3
crossoverJie <crossoverjie@gmail.com>
2020-06-29 00:28:50 +0800

    cim.client.count

--- cim-pressure-test/src/main/java/com/crossoverjie/cim/PressureApplication.java
+++ cim-pressure-test/src/main/java/com/crossoverjie/cim/PressureApplication.java
@@ -5,6 +5,7 @@ import com.crossoverjie.cim.thread.ConnectJob;
 import com.crossoverjie.cim.vo.req.LoginReqVO;
 import com.crossoverjie.cim.vo.res.CIMServerResVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -28,6 +29,9 @@ public class PressureApplication implements CommandLineRunner {
     @Autowired
     private LoginService loginService ;
 
+    @Value("${cim.client.count}")
+    private int clientCount ;
+
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(PressureApplication.class);
         application.addListeners(new ApplicationPidFileWriter());
@@ -40,7 +44,7 @@ public class PressureApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         CIMServerResVO.ServerInfo serverInfo = loginService.userLogin();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < clientCount; i++) {
             ConnectJob connectJob = new ConnectJob(new LoginReqVO(Long.valueOf(i), "abc" + i), serverInfo);
             threadPoolExecutor.execute(connectJob);
         }
