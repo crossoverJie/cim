e951ade824a9064056079e0b6be3dfb9f10f9c4f
crossoverJie <crossoverJie@gmail.com>
2024-09-08 21:09:24 +0800

    comment unless code

--- cim-common/src/test/java/com/crossoverjie/cim/common/core/proxy/RpcProxyManagerTest.java
+++ cim-common/src/test/java/com/crossoverjie/cim/common/core/proxy/RpcProxyManagerTest.java
@@ -10,9 +10,7 @@ import okhttp3.OkHttpClient;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 class RpcProxyManagerTest {
 
     @Test
