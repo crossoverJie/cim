473ccab1e7e102ff9454941df2c42c5dfaffea68
crossoverJie <crossoverJie@gmail.com>
2025-06-04 14:02:34 +0800

    refactor: simplify OfflineMsgReqVO by using Lombok's @Data annotation

--- cim-rout-api/src/main/java/com/crossoverjie/cim/route/api/vo/req/OfflineMsgReqVO.java
+++ cim-rout-api/src/main/java/com/crossoverjie/cim/route/api/vo/req/OfflineMsgReqVO.java
@@ -3,6 +3,7 @@ package com.crossoverjie.cim.route.api.vo.req;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import lombok.Data;
 
 /**
  * @author zhongcanyu
@@ -10,24 +11,11 @@ import lombok.Builder;
  * @description
  */
 @Builder
+@Data
 public class OfflineMsgReqVO {
 
     @NotNull(message = "userId can't be null")
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "message received userId", example = "1545574049323")
     private Long receiveUserId ;
 
-    public OfflineMsgReqVO() {
-    }
-
-    public OfflineMsgReqVO(Long receiveUserId) {
-        this.receiveUserId = receiveUserId;
-    }
-
-    public Long getReceiveUserId() {
-        return receiveUserId;
-    }
-
-    public void setReceiveUserId(Long receiveUserId) {
-        this.receiveUserId = receiveUserId;
-    }
 }
